d528357b44ad4f866f7b8fd2caa41a123d6b6b9a
m-darawsheh <mohamaddarawsha2005@gmail.com>
2024-11-27 09:59:17 +0300

    feat: get max func

--- algo.c
+++ algo.c
@@ -14,6 +14,16 @@ t_node *get_min(t_node *stack)
     
     return (min);
 }
+t_node  *get_max(t_node *stack)
+{
+    t_node  *max = stack;
+    while (stack)
+    {
+        if (stack->data > max->data)
+            max = stack;
+        stack = stack->next;
+    }
+}
 
 t_node *get_last(t_node *stack)
 {
@@ -31,12 +41,13 @@ void    algo(t_node **a, t_node **b)
 {
     push_to_node(a, b);
     push_to_node(a, b);
+    push_to_node(a, b);
+    rotate_node(b);
+    rotate_node(b);
+
+    push_to_node(a, b);
     rotate_node(b);
     push_to_node(a, b);
-
-    push_to_node(a, b);
-    // rotate_node(b);
-
     // rotate_node(b);
     // rotate_node(b);
     // rotate_node(b);
@@ -67,28 +78,31 @@ void    algo(t_node **a, t_node **b)
     t_node *min = get_min(*b);
     int min_index = find_index(*b, min->data);
     t_node *last_a = get_last(*a);
-
+    t_node  *max = get_max(*b);
     int rotation = 0;
 
     printf("min %d index: %d\n", min->data, min_index);
-
-    while (last_a->data > min->data)
+    if (max->data > last_a->data)
     {
-        // rotation++;
-        if (min->prev)
-            min = min->prev;
-        else
+        while (last_a->data > min->data)
         {
-            min = get_last(*b);
-            // rotation = 0;
-            // break;
+         // rotation++;
+            if (min->prev)
+                min = min->prev;
+            else
+            {
+                min = get_last(*b);
+                // rotation = 0;
+                // break;
+            }
         }
     }
+else 
+    printf("hi");
+    // rotation = calc_rotation(node_len(*b), find_index(*b, min->data));
 
-    rotation = calc_rotation(node_len(*b), find_index(*b, min->data));
-
-    printf("min %d\n", min->data);
-    printf("rotation %d\n", rotation);
+    // printf("min %d\n", min->data);
+    // printf("rotation %d\n", rotation);
 
 
 
